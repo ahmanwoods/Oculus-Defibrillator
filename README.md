d5afe94042971f743fe7f57e8f74c8d70e97b81d
Ahman Woods <ahman.woods@gmail.com>
2021-03-05 03:15:38 -0700

    OAuth token is now automagically grabbed from your Oculus AppData. MITM'ing traffic to acquire tokens is no longer needed!

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# This fix is now built into revive
-This script is no longer needed as Revive now has this functionality built in as of version 2.0. Simply sign into the Revive dashboard using your Oculus information and the rest is done for you.
 # Oculus-Defibrillator
 Fix for multiplayer when launching Oculus VR titles outside of the official client. The Oculus Platform API requires a "heartbeat" sent via the Oculus desktop client to enable multiplayer features. Currently, when launching Oculus applications outside of the desktop client without a valid headset configuration (eg, client reports pending hardware issue), the client doesnt report the correct current App ID in its heartbeat requests. This is the case when launching applications via compatibility layers such as Revive or PiTool.
 # Configuration
 1. Install Python
 2. Install requests (pip install requests)
-3. Acquire your Oculus desktop client OAuth token. Can be found under access_token in the body of graph.oculus.com requests. Fiddler is a good application for this. Enabling HTTPS Decryption is required.
-4. Configure script with OAuth token.
-5. Configure script with game App ID. Can be found at on the Oculus store at: oculus(dot)com/experiences/rift/appid
+3. Configure script with game App ID. Can be found at on the Oculus store at: oculus(dot)com/experiences/rift/appid
 # Usage
-1. Start script with python oculus_heartbeat.py
+1. Start script
 2. A response with "current_room_id" indicates it is working correctly
 3. Script must be running while you're in game
 4. ?????
--- oculus_heartbeat.py
+++ oculus_heartbeat.py
@@ -1,26 +1,73 @@
-import requests
-import time
+import sqlite3, tempfile, shutil, os, time, requests
 
-#Oculus client Oauth token. Can be acquired with Fiddler under access_token. Needs HTTPS decryption. 
-oauth = ''
+def get_oauth_token():
+	try:
+		#SQLite 3 DB with data used for offline more stored here
+		db = os.path.expandvars(r'%APPDATA%\Oculus\sessions\_oaf\\' + 'data.sqlite')
+		#DB is locked when Oculus app is open so make a temp copy of the DB to read from
+		file, path = tempfile.mkstemp()
+		shutil.copy2(db, path)        
+		with os.fdopen(file, 'w') as tmp:
+			#Connect to the temp DB   
+			sqlite = sqlite3.connect(path)
+			cur = sqlite.cursor()
+			sql_bytes = ''
 
-#Heartbeat API URL
-heartbeat = "https://graph.oculus.com/user_heartbeat?access_token=" + oauth
+			#This query should return 1 tuple with the last entry being a byte string, this contains our token.
+			for row in cur.execute("SELECT * FROM 'Objects' WHERE hashkey = '__OAF_OFFLINE_DATA_KEY__'"):
+				if len(row) > 0:
+					for val in row:
+						if type(val) is bytes:
+							sql_bytes = val
+				else:
+					raise Exception("No __OAF_OFFLINE_DATA_KEY__ found in db")
 
-#Oculus App ID. Can be acquired from oculus store at https://www.oculus.com/experiences/rift/appid
-app_id = 1360938750683878 
-#1360938750683878 for Stormland, 1369078409873402 for Echo VR
+			#The token we need is stored in 'last_valid_auth_token'
+			if sql_bytes != '':
+				offset = sql_bytes.find(b'last_valid_auth_token') + 31
+			else:
+				raise Exception("No byte array values found in __OAF_OFFLINE_DATA_KEY__")
 
-#Paramaters for API request. Chance for issues with rich presence but I'm not sure which titles use it.
-params = { 'current_status' : 'ONLINE', 'app_id_override' : app_id, 'in_vr' : 'true'}
+			#Extract the token
+			if offset != -1:
+				token = sql_bytes[offset : offset + 98]
+				return token.decode('ascii')
+			else:
+				raise Exception("last_valid_auth_token not found in db")
+	except sqlite3.OperationalError:
+		print("Error opening sqlite3 db")
+	finally:
+		#The db may or may not be open depending on the state of the script
+		try:
+			sqlite.close()
+		except:
+			pass
+		#Always remove the temp file we created
+		os.remove(path)
 
 def main():
+	#Oculus client Oauth token. 
+	oauth = get_oauth_token()
+
+	#Heartbeat API URL
+	heartbeat_url = "https://graph.oculus.com/user_heartbeat?access_token=" + oauth
+
+	#Oculus App ID. Can be acquired from oculus store at https://www.oculus.com/experiences/rift/appid
+	app_id = 1360938750683878 
+	#1360938750683878 for Stormland, 1369078409873402 for Echo VR
+
+	#Paramaters for API request. Chance for issues with rich presence but I'm not sure which titles use it.
+	params = { 'current_status' : 'ONLINE', 'app_id_override' : app_id, 'in_vr' : 'true'}
+
 	while True:
-		response = requests.post(heartbeat, params)
-		print("Heartbeat submitted")
-		#Response should contain current_room_id if working correctly (at least for Stormland)
-		print(response.content)
-		#Oculus client sends heartbeats every 10 seconds when in game
-		time.sleep(10)
-	
-main()
+		try:
+			r = requests.post(heartbeat_url, params)
+			#200 on successful post
+			if r.status_code == 200:
+				print("Heartbeat submitted successfully with response: {}".format(r.text))
+				time.sleep(10)
+		except Exception as e:
+			print(e)
+			break
+if __name__ == "__main__":
+	main()    
